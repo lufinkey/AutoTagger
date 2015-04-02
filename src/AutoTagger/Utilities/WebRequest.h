0e526e4138ae9df780cccc49cda9ec3f9ac74433
lufinkey <luisfinke@gmail.com>
2015-04-02 12:54:36 -0400

    Initial Commit
    Initial Commit

---
+++ src/AutoTagger/Utilities/WebRequest.h
@@ -0,0 +1,16 @@
+
+#pragma once
+
+#include <string>
+
+namespace autotagger
+{
+	class WebRequest
+	{
+	public:
+		typedef void(*LoadFinishCallback)(const std::string&);
+		
+		static std::string getContentsOfURL(const std::string&url);
+		static void loadContentsOfURL(const std::string&url, LoadFinishCallback callback);
+	};
+}
